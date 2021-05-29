8a72c9d63e56dd5b20e8c6ce00c7652d6d4f1362
ramyarangan <ramyar@stanford.edu>
2021-05-29 16:34:53 -0700

    Starting cnn model

---
+++ code/cnn_model.py
@@ -0,0 +1,97 @@
+"""
+Example usage: python lstm_model.py ../data/train_dev_test/train.csv ../data/train_dev_test/dev.csv
+"""
+import sys
+import pandas as pd
+import numpy as np
+
+import tensorflow as tf
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Activation, Dropout, Input, Conv1D
+from tensorflow.keras.layers import LSTM, Bidirectional, BatchNormalization
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.backend import int_shape
+
+import wandb
+from wandb.keras import WandbCallback
+
+from utils import get_X_Y
+
+# Hyperparameters
+ALPHA = 0.001
+EPOCHS = 10
+BATCH_SIZE = 64
+
+# Disables cuDNN until the Driver is updated
+# tf.compat.v1.disable_eager_execution()
+
+train_filename = sys.argv[1]
+train_df = pd.read_csv(train_filename)
+
+dev_filename = sys.argv[2]
+dev_df = pd.read_csv(dev_filename)
+
+
+def model(input_shape):    
+    X_input = Input(shape = input_shape)
+
+    # Conv layer
+    X = Conv1D(filters=128, kernel_size=15, strides=1)(X_input) 
+    X = BatchNormalization()(X)                           
+    X = Activation("relu")(X)                                
+
+    X = Conv1D(filters=64, kernel_size=15, strides=1)(X_input) 
+    X = BatchNormalization()(X)                           
+    X = Activation("relu")(X) 
+
+    # Needed for running LSTM layer on GPU
+    batch_shape = (BATCH_SIZE, int_shape(X)[1], int_shape(X)[2])
+    
+    # Two Bidirectional LSTM layers
+    X = Bidirectional(LSTM(units=64, batch_input_shape=batch_shape, \
+    	return_sequences=True))(X)
+    X = Dropout(rate=0.8)(X)                                 
+    X = BatchNormalization()(X)                           
+
+    # Needed for running LSTM layer on GPU
+    batch_shape = (BATCH_SIZE, int_shape(X)[1], int_shape(X)[2])
+     
+    X = Bidirectional(LSTM(units=64, batch_input_shape=batch_shape, \
+    	return_sequences=True))(X)
+    X = Dropout(rate=0.8)(X)                               
+    X = BatchNormalization()(X)                             
+    
+    # Fully connected layers
+    fc_sizes = [64, 1]
+    for fc_size in fc_sizes: 
+    	X = Dense(units=fc_size, activation='relu')(X)
+
+    model = Model(inputs = X_input, outputs = X)
+    
+    return model  
+
+train_X, train_Y = get_X_Y(train_df)
+dev_X, dev_Y = get_X_Y(dev_df)
+
+wandb.init(project='splicing', config={'learning_rate': ALPHA, 
+	'epochs': EPOCHS,
+	'batch_size': BATCH_SIZE,
+	'loss_function': 'mean_squared_error',
+	'architecture': 'bi-lstm',
+	'dataset': 'fullseq_all'
+	})
+
+model = model(input_shape = (train_X.shape[1], train_X.shape[2]))
+model.summary()
+
+opt = Adam(learning_rate=ALPHA, beta_1=0.9, beta_2=0.999, decay=0.01)
+model.compile(loss='mean_squared_error', optimizer=opt, metrics=["accuracy"])
+
+model.fit(train_X, train_Y, validation_data=(dev_X, dev_Y), 
+	callbacks=[WandbCallback()], batch_size=BATCH_SIZE, epochs=EPOCHS)
+model.save("lstm_model.h5")
+
+# loss, acc = model.evaluate(dev_X, dev_Y)
+# print("Dev set accuracy = ", acc)
+
+# predictions = model.predict(x)
