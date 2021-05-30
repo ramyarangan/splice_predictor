b4f258a9d88579b823d033acd828a1d585e2b248
ramyarangan <ramyar@stanford.edu>
2021-05-29 17:03:18 -0700

    Added flatten layer to fix network architecture

--- code/error_analysis.py
+++ code/error_analysis.py
@@ -1,3 +1,22 @@
+"""
+Example usage: python error_analysis.py ../data/train_dev_test/dev.csv
+"""
+import sys
+import pandas as pd
+import tensorflow as tf 
 
+from utils import get_X_Y
 
 model_file = "lstm_model.h5"
+
+model = tf.keras.models.load_model("trained_models/lstm_model.h5")
+model.summary()
+
+dev_filename = sys.argv[1]
+dev_df = pd.read_csv(dev_filename)
+dev_X, dev_Y = get_X_Y(dev_df)
+
+predictions = model.predict(dev_X)
+print(predictions.shape)
+#print(predictions[:5])
+print(dev_Y[:5])
